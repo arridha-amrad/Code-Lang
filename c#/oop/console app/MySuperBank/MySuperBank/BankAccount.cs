521dcb9ee24d582096db0e1570dc86eceedc609f
arridha-amrad <arridhaamad@gmail.com>
2023-08-07 19:55:47 +0700

    ok

---
+++ c#/oop/console app/MySuperBank/MySuperBank/BankAccount.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        public string Number { get; } = string.Empty;  
+        public string Owner { get; set; }   
+        public decimal Balance 
+        {
+            get
+            {
+                decimal balance = 0;
+                
+                foreach (var item in this.allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                
+                return balance;
+            }
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransactions = new();
+
+
+        public BankAccount(string name, decimal amount)
+        {
+            this.Owner = name;
+            MakeDeposit(amount, DateTime.Now, "Initial Deposit");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            Console.WriteLine($"Deposit : {amount} for {note}");
+            Transaction deposit = new(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note) 
+        {
+            if(amount <= 0) { 
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if(Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficeient funds for this withdrawal");
+            }
+            Console.WriteLine($"Withdraw : {amount} for {note}");
+            Transaction withdrawal = new(-amount, date, note);
+            allTransactions.Add(withdrawal);        
+        }
+
+        public string GetAccountHistory()
+        {
+            StringBuilder report = new();
+            // HEADER
+            report.AppendLine("Date\t\tAmount\tNote");
+            foreach(Transaction transaction in allTransactions)
+            {
+                // ROWS
+                report.AppendLine($"{transaction.Date.ToShortDateString()}\t{transaction.Amount}\t{transaction.Notes}");
+            }
+            return report.ToString();
+        }
+    }
+}
---
+++ c#/oop/console app/MySuperBank/MySuperBank/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MySuperBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new("Alex", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+
+            account.MakeWithdrawal(100, DateTime.Now, "Buying car");
+            Console.WriteLine($"Account {account.Number} for {account.Owner} with balance: {account.Balance}");
+
+            account.MakeDeposit(200, DateTime.Now, "Sisa THR");
+            Console.WriteLine($"Account {account.Number} for {account.Owner} with balance: {account.Balance}");
+
+            string report = account.GetAccountHistory();
+            Console.WriteLine(report);
+        }
+    }
+}
